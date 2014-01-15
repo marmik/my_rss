d8a69fcf79fef3b8ffa7d3acd80903711e47abb0
valik <valik@valik-pc.(none)>
2014-01-15 23:28:32 +0200

    c

---
+++ src/client/xml.java
@@ -0,0 +1,15 @@
+package client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: valik
+ * Date: 1/15/14
+ * Time: 11:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class xml {
+
+    public static void main(String[] args) {
+        System.out.println("yo");
+    }
+}
